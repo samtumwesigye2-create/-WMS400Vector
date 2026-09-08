7bb876abfb6fa7e2b30cc5f44d52eeda7978408b
samtumwesigye2-create <samtumwesigye2@gmail.com>
2026-09-08 16:51:31 -0400

    Add warehouse control and reverse logistics layers (#1)
    * Add warehouse capacity, PO linkage, and reverse logistics layers
    
    * Expose warehouse control layers

--- entrypoint.py
+++ entrypoint.py
@@ -1,3 +1,6 @@
 from app import app
 from nexus_bridge import router as nexus_router
+from warehouse_control import router as warehouse_control_router
+
 app.include_router(nexus_router)
+app.include_router(warehouse_control_router)

f770b3de50cb440e90458d621985f433efd0c1bd
samtumwesigye2-create <samtumwesigye2@gmail.com>
2026-09-08 19:36:47 -0400

    Mount live inventory intelligence router

--- entrypoint.py
+++ entrypoint.py
@@ -3,8 +3,10 @@ from nexus_bridge import router as nexus_router
 from warehouse_control import router as warehouse_control_router
 from cold_chain import router as cold_chain_router
 from ugatu_returns import router as ugatu_returns_router
+from inventory_costs import router as inventory_costs_router
 
 app.include_router(nexus_router)
 app.include_router(warehouse_control_router)
 app.include_router(cold_chain_router)
 app.include_router(ugatu_returns_router)
+app.include_router(inventory_costs_router)

dfe1c88633655a2241476d8e311abfa41991eecc
samtumwesigye2-create <samtumwesigye2@gmail.com>
2026-09-08 17:02:17 -0400

    Add cold-chain custody layer
    * Add cold-chain custody logistics layer
    
    * Mount cold-chain custody routes

--- entrypoint.py
+++ entrypoint.py
@@ -1,6 +1,8 @@
 from app import app
 from nexus_bridge import router as nexus_router
 from warehouse_control import router as warehouse_control_router
+from cold_chain import router as cold_chain_router
 
 app.include_router(nexus_router)
 app.include_router(warehouse_control_router)
+app.include_router(cold_chain_router)

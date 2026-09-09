d8624b8e83b3e6e54081d53a220e6a5df1a7327e
samtumwesigye2-create <samtumwesigye2@gmail.com>
2026-09-08 20:20:24 -0400

    Mount VECTOR KPI publisher

--- entrypoint.py
+++ entrypoint.py
@@ -5,6 +5,7 @@ from cold_chain import router as cold_chain_router
 from ugatu_returns import router as ugatu_returns_router
 from inventory_costs import router as inventory_costs_router
 from material_documents import router as material_documents_router
+from vector_kpis import router as vector_kpis_router
 
 app.include_router(nexus_router)
 app.include_router(warehouse_control_router)
@@ -12,3 +13,4 @@ app.include_router(cold_chain_router)
 app.include_router(ugatu_returns_router)
 app.include_router(inventory_costs_router)
 app.include_router(material_documents_router)
+app.include_router(vector_kpis_router)

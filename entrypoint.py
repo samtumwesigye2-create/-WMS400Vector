1926df6de4eed28635a6900aeadd8ea352d3e4bf
samtumwesigye2-create <samtumwesigye2@gmail.com>
2026-09-20 03:33:58 -0400

    Wire VECTOR command dashboard analytics routes

--- entrypoint.py
+++ entrypoint.py
@@ -8,6 +8,7 @@ from material_documents import router as material_documents_router
 from vector_kpis import router as vector_kpis_router
 from ugatu_fulfillment import router as ugatu_fulfillment_router
 from inventory_methods import router as inventory_methods_router
+from warehouse_analytics import router as warehouse_analytics_router
 
 app.include_router(nexus_router)
 app.include_router(warehouse_control_router)
@@ -19,3 +20,4 @@ app.include_router(vector_kpis_router)
 app.include_router(ugatu_fulfillment_router)
 
 app.include_router(inventory_methods_router)
+app.include_router(warehouse_analytics_router)
